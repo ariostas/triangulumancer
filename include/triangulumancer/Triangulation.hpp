4f88ef3b1d05edf23ab0fa2c1c64663f4a5e0549
Andres Rios Tascon <ariostas@gmail.com>
2025-05-22 06:45:32 -0400

    tmp

---
+++ include/triangulumancer/Triangulation.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <optional>
+#include <string>
+#include <memory>
+
+namespace triangulumancer {
+
+    class PointConfiguration;
+
+    struct Triangulation {
+        std::shared_ptr<PointConfiguration> pc;
+
+        pybind11::array_t<int64_t> simplices_;
+
+        // Constructors
+        Triangulation() = delete;
+        Triangulation(std::shared_ptr<PointConfiguration> pc) : pc(pc) {}
+        Triangulation(std::shared_ptr<PointConfiguration> pc, pybind11::array_t<int64_t> simplices_) : pc(pc), simplices_(simplices_) {}
+
+        // Basic info
+        size_t n_simplices() const;
+        size_t dim() const;
+        std::string repr() const;
+        pybind11::array_t<int64_t> simplices();
+
+        // TOPCOM functionality
+        // neighbor_triangulations
+    };
+
+}
