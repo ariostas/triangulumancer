c3eef4aecc621c4438729de5a65462aa6cd8ccc5
Andres Rios Tascon <ariostas@gmail.com>
2026-03-28 10:52:55 -0400

    Fixed repr

--- src/triangulumancer/Flip.cpp
+++ src/triangulumancer/Flip.cpp
@@ -7,18 +7,15 @@ Flip::Flip(Triangulation const &pre, Triangulation const &post)
 
 std::string Flip::repr() const {
   std::string msg;
-  msg = "A bistellar flip between a triangulation with " +
-        std::to_string(pre.n_simplices()) +
-        " simplices and a triangulation with " +
-        std::to_string(post.n_simplices()) + " simplices, both of a " +
+  msg = "A bistellar flip of a triangulation of a " +
         std::to_string(pre.dim()) + "-dimensional ";
   bool is_pc = pre.pvc.pvc_data->config_type == ConfigurationType::Point;
   if (is_pc) {
     msg = msg + "point configuration with " + std::to_string(pre.pvc.n_pv()) +
-          " points.";
+          " points";
   } else {
     msg = msg + "vector configuration with " + std::to_string(pre.pvc.n_pv()) +
-          " vectors.";
+          " vectors";
   }
   return msg;
 }
